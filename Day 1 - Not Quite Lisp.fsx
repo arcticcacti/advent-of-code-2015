cf50166ce895026d23e595fa90a0bf891115f1c7
Lee Holmes <leeholmesmail+github@gmail.com>
2016-12-06 19:07:16 +0000

    Day 1 part 2

--- Day 1 - Not Quite Lisp.fsx
+++ Day 1 - Not Quite Lisp.fsx
@@ -2,19 +2,39 @@
 
 let getInput() = AdventUtils.inputFromFile "day_1.txt" |> Array.head
 
+/// move from a position by parsing a movement char
 let move current dir =
     match dir with
     | '(' -> current + 1
     | ')' -> current - 1
     | _ -> failwithf "Unrecognised direction char %c" dir
 
+/// get the final floor number after following directions from floor 0
 let followDirections = Seq.fold move 0
 
+/// get the step in a set of directions where the basement is entered 
+let basementEntered = Seq.scan move 0 >> Seq.findIndex ((=) -1)
+
+
+//
+// main functions
+//
 
 let part1() =
     getInput()
     |> followDirections
-    |> printfn "Part 1 - final floor: %d" 
+    |> printfn "Part 1 - final floor: %d"
+
+
+let part2() =
+    getInput()
+    |> basementEntered
+    |> printfn "Part 2 - basement entered on step %d"
+
+
+//
+// tests
+//
 
 let test1() =
     let testData =
@@ -32,10 +52,20 @@ let test1() =
         let shouldBe = AdventUtils.printTestResult (sprintf "1 - %s" data) (=)
         let result = followDirections data
         result |> shouldBe expected
+    testData |> Seq.iter runTest
 
+let test2() =
+    let testData = [| ")", 1; "()())", 5 |]
+    let runTest (data, expected) =
+        let shouldBe = AdventUtils.printTestResult (sprintf "2 - %s" data) (=)
+        let result = basementEntered data
+        result |> shouldBe expected
     testData |> Seq.iter runTest
 
 
 
+
 part1()
+part2()
 test1()
+test2()
