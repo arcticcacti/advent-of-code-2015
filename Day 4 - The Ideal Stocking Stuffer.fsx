b7664a57e74750d9e20abd8541ee5aa0afec4a2e
Lee Holmes <leeholmesmail+github@gmail.com>
2016-12-07 20:55:19 +0000

    Day 4 part 1

---
+++ Day 4 - The Ideal Stocking Stuffer.fsx
@@ -0,0 +1,53 @@
+open System.Security.Cryptography
+open System.Text
+#load "AdventUtils.fsx"
+
+let getInput() = "bgvyzdsv"
+
+let md5 = MD5.Create()
+
+
+/// combine two elements and produce its MD5 hash in hexadecimal
+let hash key number =
+    key + string number
+    |> Encoding.ASCII.GetBytes
+    |> md5.ComputeHash 
+    |> Seq.map (fun c -> c.ToString("X2"))
+    |> Seq.reduce (+)
+
+
+let startsWith prefix = Seq.zip prefix >> Seq.forall (fun (x,y) -> x = y)
+
+/// the lowest integer paired with a key that produces a valid AdventCoin hash
+let mineCoin key =
+    let performHash = hash key
+    Seq.initInfinite ((+)1)
+    |> Seq.find (performHash >> startsWith "00000")
+
+
+
+//
+// Main functions
+//
+
+let part1() =
+    getInput()
+    |> mineCoin
+    |> printfn "Part 1 - found hash with suffix: %d"
+
+
+// Tests
+
+let test1() =
+    let testData = [|
+        "abcdef", "609043";
+        "pqrstuv", "1048970"
+    |]
+    let test = AdventUtils.testResultIsExpected "1" (mineCoin >> string)
+    testData |> Array.iter test
+
+
+part1()
+
+test1()
+1
