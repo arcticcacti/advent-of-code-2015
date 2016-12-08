3809bd02fc002d0b78c09f0c6d4925165b6084ff
Lee Holmes <leeholmesmail+github@gmail.com>
2016-12-08 18:41:55 +0000

    Day 5 part 2

--- Day 5 - Doesnt He Have Intern-Elves For This.fsx
+++ Day 5 - Doesnt He Have Intern-Elves For This.fsx
@@ -18,14 +18,40 @@ let doesNotContainAnyOf pairs str =
     let allPairs = str |> Seq.pairwise |> Seq.map (fun (x,y) -> string x + string y)
     countAnyOf allPairs pairs = 0
 
+/// whether a string has a pair of identical chars separated by any other, e.g. XYX
+let hasXYXrepeatingLetter =
+    Seq.windowed 3 >> Seq.exists (fun arr -> arr.[0] = arr.[2])
+
+let hasADoubleNonOverlappingLetter str =
+    /// check if any pair has a match in the list, ignoring the adjacent pair
+    let rec containsNonAdjacentPair =
+        function
+        | [] | [_] | [_;_]
+            -> false
+        | x::y::xs
+            -> if List.contains x xs then
+                   true
+               else
+                   containsNonAdjacentPair (y::xs)
+
+    str |> Seq.pairwise |> List.ofSeq |> containsNonAdjacentPair
+
 
 /// Whether a string is Naughty or Nice according to Santa's rules
-let naughtyOrNice (str:string) =
+let naughtyOrNice str =
     let isNice =
         str |> countAnyOf ['a'; 'e'; 'i'; 'o'; 'u'] >= 3
         && str |> hasADoubleLetter
         && str |> doesNotContainAnyOf ["ab"; "cd"; "pq"; "xy"]
     if isNice then Nice else Naughty
+
+let naughtyOrNice2 str =
+    let isNice =
+        str |> hasADoubleNonOverlappingLetter
+        && str |> hasXYXrepeatingLetter
+    if isNice then Nice else Naughty
+
+let countNice f = Seq.map f >> Seq.filter ((=) Nice) >> Seq.length
 
 //
 // Main
@@ -33,10 +59,13 @@ let naughtyOrNice (str:string) =
 
 let part1() =
     getInput()
-    |> Seq.map naughtyOrNice
-    |> Seq.filter ((=) Nice)
-    |> Seq.length
+    |> countNice naughtyOrNice
     |> printfn "Part 1 - %d strings are nice"
+
+let part2() =
+    getInput()
+    |> countNice naughtyOrNice2
+    |> printfn "Part 2 - %d strings are nice"
 
 // Tests
 
@@ -51,8 +80,19 @@ let test1() =
     let test = AdventUtils.testResultIsExpected "1" naughtyOrNice
     testData |> Array.iter test
 
+let test2() =
+    let testData = [|
+        "qjhvhtzxzqqjkmpb", Nice;
+        "xxyxx", Nice;
+        "uurcxstgmygtbstg", Naughty;
+        "ieodomkazucvgmuy", Naughty
+    |]
+    let test = AdventUtils.testResultIsExpected "2" naughtyOrNice2
+    testData |> Array.iter test
 
 part1()
+part2()
 
 test1()
+test2()
 1
